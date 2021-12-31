7aaea594f09ca01cf26ac148ffd3c7b525864a1e
Silva, Eduardo Cipriano <edfcsx@edfcsx.(none)>
2021-12-30 23:56:50 -0300

    implements a textureManager

---
+++ src/TextureManager.h
@@ -0,0 +1,17 @@
+#ifndef TEXTUREMANAGER_H
+#define TEXTUREMANAGER_H
+
+#include "./Game.h"
+
+class TextureManager {
+    public:
+        static SDL_Texture* LoadTexture(const char* filename);
+        static void Draw(
+            SDL_Texture* texture,
+            SDL_Rect sourceRectangle,
+            SDL_Rect destinationRectangle,
+            SDL_RendererFlip
+        );
+};
+
+#endif
