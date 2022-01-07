c0ad01c8ba671ac3336ed8cb27474b76c72b6581
Silva, Eduardo Cipriano <edfcsx@edfcsx.(none)>
2022-01-07 17:42:17 -0300

    add function to limit players on edges

--- src/Components/TransformComponent.h
+++ src/Components/TransformComponent.h
@@ -5,6 +5,7 @@
 #include "../EntityManager.h"
 #include "../../lib/glm/glm.hpp"
 #include "../Game.h"
+#include "../Constants.h"
 
 class TransformComponent: public Component {
     public:
@@ -29,11 +30,30 @@ class TransformComponent: public Component {
         void Update(float deltaTime) override {
             // Todo:
             // update the position/velocity as a function of deltatime
-            position.x += velocity.x * deltaTime;
-            position.y += velocity.y * deltaTime;
+            
+            if (!InTheEdge(deltaTime)) {
+                position.x += velocity.x * deltaTime;
+                position.y += velocity.y * deltaTime;
+            }
         }
 
         void Render() override {}
+
+        bool InTheEdge (float deltaTime) {
+            glm::vec2 destPos = glm::vec2(
+                static_cast<int>((position.x + (width * scale)) + (velocity.x * deltaTime)),
+                static_cast<int>((position.y + (height * scale)) + (velocity.y * deltaTime))
+            );
+
+            bool axisXInEdge = destPos.x <= (width * scale) || destPos.x >= WINDOW_WIDTH;
+            bool axisYInEdge = destPos.y <= (height * scale) || destPos.y >= WINDOW_HEIGHT;
+
+            if (axisXInEdge || axisYInEdge) {
+                return true;
+            }
+
+            return false;
+        }
 };
 
 #endif
