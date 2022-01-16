1a31fe646e2aaa79f74ca3b11ebd7bf37d846749
Silva, Eduardo Cipriano <edfcsx@gmail.com>
2022-01-16 10:41:39 -0300

    Add linux install dependencies to run a project

---
+++ dev_dependencies_install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt install libsdl2-dev -y
+sudo apt install libsdl2-image-dev -y
+sudo apt install libsdl2-ttf-dev -y
+sudo apt install libsdl2-mixer-dev -y
+sudo apt install liblua5.3-dev -y
